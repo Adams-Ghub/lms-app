a7a02735fd9615327f4809eb4c6db259baf6e90f
Adams-Ghub <bamfoadamsf@gmail.com>
2023-11-11 21:05:42 +0000

    Add a little styling to the CourseListItem component

--- src/components/CourseListItem.jsx
+++ src/components/CourseListItem.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const CourseListItem = ({ name, instructor, duration }) => {
+const CourseListItem = ({ name, instructor, duration,location }) => {
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <View>
-        <Text>{name}</Text>
+        <Text style={styles.courseNameText}>{name}</Text>
       </View>
-      <View>
+      <View style={styles.instructorDurationContainer}>
         <Text>{instructor}</Text>
         <Text>{duration}</Text>
+        <Text>{location}</Text>
       </View>
     </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 0,
+    backgroundColor: '#eee',
+    width: '100%',
+    marginBottom: 5,
+    padding: 6,
+    borderRadius:7
+  },
+  instructorDurationContainer: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+  courseNameText:{
+    fontSize:20,
+    fontWeight:'500'
+  }
+});
 
 export default CourseListItem;
