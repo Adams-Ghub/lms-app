afa694fd54b8af69f0fadc68c8f0686b966c1785
Adams-ghub <bamfoadamsf@gmail.com>
2023-11-12 09:06:04 +0000

    Import main navigation

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import CourseList from './src/screens/CourseList';
+import {NavigationContainer} from '@react-navigation/native'
+import MainNavigation from './src/navigation/MainNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CourseList />
-    </View>
+    <NavigationContainer>
+      <MainNavigation/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
